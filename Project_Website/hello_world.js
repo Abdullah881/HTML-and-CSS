6d22a5fdb7fb6fe28c12eae5963a8874363f814f
Abdullah881 <abdullah.altaf881@gmail.com>
2023-08-11 12:37:18 +0500

    Create hello_world.js

---
+++ Project_Website/hello_world.js
@@ -0,0 +1,10 @@
+var greetings = "Hello World";
+    console.log(greetings);
+
+var greetings2 = "Welcome to node.js";
+console.log(greetings2);
+
+var sentence1 = "How to Debug code with a debugger "
+console.log(sentence1);
+
+var x =6
